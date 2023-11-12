e0c7dded132d7d6051ffbbb2273a04c0944f6b97
Bourhane Younes <bourhaneyounes1@gmail.com>
2023-11-12 20:54:23 +0100

    refactored and change project structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"strconv"
+import(
+  "fmt"
+  "net/http"
+  "strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request){
@@ -34,18 +33,4 @@ func snippetCreate(w http.ResponseWriter, r *http.Request){
     return
   }
   w.Write([]byte("create a new snippet"))
-}
-
-func main(){
- 
-  mux := http.NewServeMux()
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
-
-
-  log.Print("Starting server on :4000")
-  err := http.ListenAndServe(":4000",mux)
-  log.Fatal(err)
-
 }
