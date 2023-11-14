4f53125c9cfdf7d970314f88b4abe1d3adc664d0
Bourhane Younes <bourhaneyounes1@gmail.com>
2023-11-14 12:26:12 +0100

    refactored code, error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main(){
- 
-  mux := http.NewServeMux()
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+type config struct {
+	addr      string
+	staticDir string
+}
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	cfg      config
+}
 
-  log.Print("Starting server on :4000")
-  err := http.ListenAndServe(":4000",mux)
-  log.Fatal(err)
+func main() {
+	var cfg config
 
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		cfg:      cfg,
+	}
+
+	srv := &http.Server{
+		Addr:     app.cfg.addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", app.cfg.addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
