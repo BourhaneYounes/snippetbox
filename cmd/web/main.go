553aa4e23fbc06c9e7cca2d3effae79fc9d6ae05
Bourhane Younes <bourhaneyounes1@gmail.com>
2023-11-14 13:32:35 +0100

    added mysql driver, flag for dsn, db pool connections

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/BourhaneYounes/snippetbox/internal/models"
 )
 
 type config struct {
 	addr      string
 	staticDir string
+	dsn       string
 }
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 	cfg      config
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -23,15 +30,28 @@ func main() {
 
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	dsn := flag.String(
+		"dsn",
+		"web:web@tcp(172.19.0.2:3306)/snippetbox?parseTime=true",
+		"MySQL data source name",
+	)
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 		cfg:      cfg,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -41,6 +61,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", app.cfg.addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
