e97a3330cf69b7d3b9e49b97a37d20f28d91f2ba
Bourhane Younes <bourhaneyounes1@gmail.com>
2023-11-16 12:26:44 +0100

    model for snippet, db connection

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -22,5 +23,44 @@ func (m *SnippetModel) Latest() ([]*Snippet, error) {
 }
 
 func (m *SnippetModel) Insert(title, content string, expires int) (int, error) {
-	stmt := ``
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+  VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	res, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+    WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	snippet := &Snippet{}
+
+	err := row.Scan(
+		&snippet.ID,
+		&snippet.Title,
+		&snippet.Content,
+		&snippet.Created,
+		&snippet.Expires,
+	)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return snippet, nil
 }
